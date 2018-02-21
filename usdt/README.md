f7cf294f6d07a401d7b359a3e663ab948bf17b79
Ruochen Lyu <kshift.lv@gmail.com>
2018-02-21 10:35:41 +0800

    add usdt/cny chart

---
+++ usdt/README.md
@@ -0,0 +1,3 @@
+# 火币 USDT / CNY 每日价格
+
+<http://kshift.me/usdt>
---
+++ usdt/run.rb
@@ -0,0 +1,14 @@
+require 'csv'
+require 'json'
+require 'date'
+
+response_buy = `curl --request GET --url 'https://api-otc.huobi.pro/v1/otc/trade/list/public?coinId=2&tradeType=1&currentPage=1&payWay=&country=&merchant=1&online=1&range=0' --header 'Cache-Control: no-cache'`
+response_sell = `curl --request GET --url 'https://api-otc.huobi.pro/v1/otc/trade/list/public?coinId=2&tradeType=0&currentPage=1&payWay=&country=&merchant=1&online=1&range=0' --header 'Cache-Control: no-cache'`
+
+CSV.open('data.csv', 'a+') do |csv|
+  csv << [
+    DateTime.parse(DateTime.now.strftime("%Y-%m-%dT%H:00%z")),
+    JSON.parse(response_buy)['data'][0]['fixedPrice'],
+    JSON.parse(response_sell)['data'][0]['fixedPrice']
+  ]
+end
