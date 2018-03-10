d52403ed23ddaf39fec4f614651c1dad61036461
Lúcio <kshift.lv+pi@gmail.com>
2018-03-11 00:03:45 +0800

    add https_proxy

--- huobi_cron/main.sh
+++ huobi_cron/main.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+export https_proxy=http://127.0.0.1:8888;export http_proxy=http://127.0.0.1:8888
 currency=`python3 peep.py`
 
 if [ ${currency} ];then
   echo ${currency}
-  ruby ~/Documents/hadax_demo/open_a_position.rb ${HUOBI_KEY} ${HUOBI_SECRET} ${HUOBI_ID} ${currency}
+  ruby ~/Documents/hadax_demo/open_a_position.rb ${HUOBI_KEY} ${HUOBI_SECRET} ${HUOBI_ID} 'btc' ${currency} 0.01
 fi
