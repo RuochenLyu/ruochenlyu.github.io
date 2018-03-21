28be1c449e28de6e9ed79c2d901e3c06d30c510f
Lúcio <kshift.lv+pi@gmail.com>
2018-03-21 09:30:03 +0800

    update data.csv 2018-03-21 09:30

--- usdt/run.rb
+++ usdt/run.rb
@@ -4,13 +4,20 @@ require 'date'
 
 response_buy = `curl --request GET --url 'https://api-otc.huobi.pro/v1/otc/trade/list/public?coinId=2&tradeType=1&currentPage=1&payWay=&country=&merchant=1&online=1&range=0' --header 'Cache-Control: no-cache'`
 response_sell = `curl --request GET --url 'https://api-otc.huobi.pro/v1/otc/trade/list/public?coinId=2&tradeType=0&currentPage=1&payWay=&country=&merchant=1&online=1&range=0' --header 'Cache-Control: no-cache'`
-response_price = `curl --request GET --url 'https://otc-api.huobipro.com/v1/otc/base/market/price' --header 'Cache-Control: no-cache'`
+buy_price = JSON.parse(response_buy)['data'][0]['fixedPrice']
+sell_price = JSON.parse(response_sell)['data'][0]['fixedPrice']
+base_price = nil
+
+if Time.new.min == 0
+  response_price = `curl --request GET --url 'http://www.apilayer.net/api/live?access_key=2fc9d3a4761e1c3cacbd2f6e0f6f205f&format=1&currencies=CNY' --header 'Cache-Control: no-cache'`
+  base_price = JSON.parse(response_price)['quotes']['USDCNY']
+end
 
 CSV.open('data.csv', 'a+') do |csv|
   csv << [
     DateTime.parse(DateTime.now.strftime("%Y-%m-%dT%H:%M%z")),
-    JSON.parse(response_buy)['data'][0]['fixedPrice'],
-    JSON.parse(response_sell)['data'][0]['fixedPrice'],
-    JSON.parse(response_price)['data'][2]['price']
+    buy_price,
+    sell_price,
+    base_price
   ]
 end
