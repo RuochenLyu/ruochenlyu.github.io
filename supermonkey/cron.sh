0b3b9546eeaf531c9d29fb842c68d31ce66a883a
Ruochen Lyu <kshift.lv@gmail.com>
2022-07-29 14:48:34 +0800

    change: delete useless files

--- supermonkey/cron.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-source ~/.zshrc
-
-ruby ./run.rb
-git commit -am "update supermonkey data $(date '+%Y-%m-%d %H:%M:%S')"
-git pull --rebase
-git push
--- usdt/README.md
+++
@@ -1,5 +0,0 @@
-# 火币 USDT / CNY 每日价格
-
-<http://kshift.me/usdt>
-
-感谢幸苦提交代码的机器人 Lucio.
